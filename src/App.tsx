d65e99ee878871826298517bfe30b1b074d6829e
Mhd Ali Akbar Sitompul <maliakbar475@gmail.com>
2026-03-19 01:56:15 +0700

    'update'

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,36 @@ import Cars from "./pages/Cars";
 import Contact from "./pages/Contact";
 import Location from "./pages/Location";
 import CarDetail from "./pages/CarDetail";
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet-async';
 
 function App() {
+  const schemaData = {
+    "@context": "https://schema.org",
+    "@type": "CarRental",
+    "name": "Rental Mobil Muezza Medan",
+    "url": "https://rentalmobilmedanmuezza.com",
+    "telephone": "+6281269299521", // Ganti dengan nomor asli
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "Jl. Garu 1 No.106, Sitirejo III, Kec. Medan Amplas",
+      "addressLocality": "Medan",
+      "addressRegion": "Sumatera Utara",
+      "addressCountry": "ID"
+    },
+    "description": "Layanan rental mobil Medan terpercaya, murah, dan lepas kunci."
+  };
+
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-neutral-50">
+        <Helmet>
+        <title>Rental Mobil Medan Murah & Terpercaya - Muezza Rental</title>
+        <meta name="description" content="Sewa mobil Medan lepas kunci atau dengan driver. Armada lengkap, harga mulai 300rb-an. Melayani area Kualanamu dan Sumatera Utara." />
+        <script type="application/ld+json">
+          {JSON.stringify(schemaData)}
+        </script>
+      </Helmet>
+
         <Helmet>
           <title>
             Rental Mobil Medan Muezza - Sewa Mobil Murah & Fleksibel
